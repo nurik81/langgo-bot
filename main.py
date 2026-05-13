8ac875c6dd90207a4ab71ca353ac2f883054358c
nurik81 <qalbdagigaplar01@gmail.com>
2026-05-13 12:31:07 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+from flask import Flask
+import threading
+
+app = Flask('')
+@app.route('/')
+def home():
+    return "Bot is running!"
+
+def run():
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
+threading.Thread(target=run).start()
 import os
 import pandas as pd
 from datetime import time
