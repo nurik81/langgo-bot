220bc0910407875db9ca7d90901e22a79c142cc1
nurik81 <qalbdagigaplar01@gmail.com>
2026-05-14 13:38:54 +0500

    Update german_words.py

--- german_words.py
+++ german_words.py
@@ -1,9 +1,8 @@
 de_data = [
-    # --- OLMOSHLAR (Menda, Senda, Uniki...) ---
-    ["men", "ich", "-", "-"], ["sen", "du", "-", "-"],
-    ["u (o'g'il)", "er", "-", "-"], ["u (qiz)", "sie", "-", "-"],
-    ["biz", "wir", "-", "-"], ["siz", "ihr/Sie", "-", "-"],
-    ["ular", "sie", "-", "-"],
-    ["meniki", "mein", "-", "-"], ["seniki", "dein", "-", "-"],
-    ["uniki (o'g'il)", "sein", "-", "-"], ["uniki (qiz)", "ihr", "-", "-"],
-    ["menda bor", "ich habe", "-", "-"], ["senda bor", "du hast", "-", "-"],
+    ["kitob", "Buch", "das", "Bücher"],
+    ["stol", "Tisch", "der", "Tische"],
+    ["stul", "Stuhl", "der", "Stühle"],
+    ["uy", "Haus", "das", "Häuser"],
+    ["karovat", "Bett", "das", "Betten"],
+    ["oyna", "Spiegel", "der", "Spiegel"]
+]
