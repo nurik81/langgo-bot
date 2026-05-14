2af78ec923d003b3d72706e04c433d4416ee660f
nurik81 <qalbdagigaplar01@gmail.com>
2026-05-14 12:30:55 +0500

    Add German vocabulary list to german_words.py

---
+++ german_words.py
@@ -0,0 +1,140 @@
+de_data = [
+    # --- OLMOSHLAR (Menda, Senda, Uniki...) ---
+    ["men", "ich", "-", "-"], ["sen", "du", "-", "-"],
+    ["u (o'g'il)", "er", "-", "-"], ["u (qiz)", "sie", "-", "-"],
+    ["biz", "wir", "-", "-"], ["siz", "ihr/Sie", "-", "-"],
+    ["ular", "sie", "-", "-"],
+    ["meniki", "mein", "-", "-"], ["seniki", "dein", "-", "-"],
+    ["uniki (o'g'il)", "sein", "-", "-"], ["uniki (qiz)", "ihr", "-", "-"],
+    ["menda bor", "ich habe", "-", "-"], ["senda bor", "du hast", "-", "-"],
+
+    # --- MEVALAR VA SABZAVOTLAR ---
+    ["olma", "Apfel", "der", "Äpfel"], ["banan", "Banane", "die", "Bananen"],
+    ["limon", "Zitrone", "die", "Zitronen"], ["uzum", "Traube", "die", "Trauben"],
+    ["qovun", "Melone", "die", "Melonen"], ["shaftoli", "Pfirsich", "der", "Pfirsiche"],
+    ["kartoshka", "Kartoffel", "die", "Kartoffeln"], ["piyoz", "Zwiebel", "die", "Zwiebeln"],
+    ["pomidor", "Tomate", "die", "Tomaten"], ["bodring", "Gurke", "die", "Gurken"],
+    ["sabzi", "Karotte", "die", "Karotten"], ["sarimsoq", "Knoblauch", "der", "-"],
+
+    # --- OILA VA QARINDOSHLAR ---
+    ["ota", "Vater", "der", "Väter"], ["ona", "Mutter", "die", "Mütter"],
+    ["o'g'il", "Sohn", "der", "Söhne"], ["qiz", "Tochter", "die", "Töchter"],
+    ["aka/uka", "Bruder", "der", "Brüder"], ["opa/singil", "Schwester", "die", "Schwestern"],
+    ["buva", "Großvater", "der", "Großväter"], ["buvi", "Großmutter", "die", "Großmütter"],
+    ["er", "Ehemann", "der", "Ehemänner"], ["xotin", "Ehefrau", "die", "Ehefrauen"],
+
+    # --- TANA A'ZOLARI ---
+    ["bosh", "Kopf", "der", "Köpfe"], ["ko'z", "Auge", "das", "Augen"],
+    ["quloq", "Ohr", "das", "Ohren"], ["burun", "Nase", "die", "Nasen"],
+    ["og'iz", "Mund", "der", "Münder"], ["qo'l", "Hand", "die", "Hände"],
+    ["oyoq", "Fuß", "der", "Füße"], ["yurak", "Herz", "das", "Herzen"],
+    ["soch", "Haar", "das", "Haare"], ["tish", "Zahn", "der", "Zähne"],
+
+    # --- KUNDALIK FE'LLAR ---
+    ["yemoq", "essen", "-", "-"], ["ichmoq", "trinken", "-", "-"],
+    ["uxlamoq", "schlafen", "-", "-"], ["uyg'onmoq", "aufwachen", "-", "-"],
+    ["bormoq", "gehen", "-", "-"], ["kelmoq", "kommen", "-", "-"],
+    ["ishlamoq", "arbeiten", "-", "-"], ["o'qimoq", "lesen", "-", "-"],
+    ["yozmoq", "schreiben", "-", "-"], ["gapirmoq", "sprechen", "-", "-"],
+    ["ko'rmoq", "sehen", "-", "-"], ["eshitmoq", "hören", "-", "-"],
+    ["sotib olmoq", "kaufen", "-", "-"], ["to'lamoq", "bezahlen", "-", "-"],
+
+    # --- VAQT VA KUNLAR ---
+    ["bugun", "heute", "-", "-"], ["ertaga", "morgen", "-", "-"],
+    ["kecha", "gestern", "-", "-"], ["hozir", "jetzt", "-", "-"],
+    ["dushanba", "Montag", "der", "-"], ["seshanba", "Dienstag", "der", "-"],
+    ["chorshanba", "Mittwoch", "der", "-"], ["payshanba", "Donnerstag", "der", "-"],
+    ["juma", "Freitag", "der", "-"], ["shanba", "Samstag", "der", "-"],
+    ["yakshanba", "Sonntag", "der", "-"],
+
+    # --- SIFATLAR ---
+    ["yaxshi", "gut", "-", "-"], ["yomon", "schlecht", "-", "-"],
+    ["issiq", "heiß", "-", "-"], ["sovuq", "kalt", "-", "-"],
+    ["katta", "groß", "-", "-"], ["kichik", "klein", "-", "-"],
+    ["chiroyli", "schön", "-", "-"], ["tez", "schnell", "-", "-"],
+    ["sekin", "langsam", "-", "-"], ["yangi", "neu", "-", "-"],
+    ["eski", "alt", "-", "-"], ["og'ir", "schwer", "-", "-"],
+    ["yengil", "leicht", "-", "-"]
+]
+    # --- UY JIOHOZLARI (Hausrat) ---
+    ["karovat", "Bett", "das", "Betten"], ["oyna (ko'zgu)", "Spiegel", "der", "Spiegel"],
+    ["muzlatgich", "Kühlschrank", "der", "Kühlschränke"], ["gilam", "Teppich", "der", "Teppiche"],
+    ["parda", "Vorhang", "der", "Vorhänge"], ["lampa", "Lampe", "die", "Lampen"],
+    ["idish", "Geschirr", "das", "-"], ["qoshiq", "Löffel", "der", "Löffel"],
+    ["vilka", "Gabel", "die", "Gabeln"], ["pichoq", "Messer", "das", "Messer"],
+    ["tovoq", "Teller", "der", "Teller"], ["stakan", "Glas", "das", "Gläser"],
+
+    # --- KIYIM-KECHAKLAR (Kleidung) ---
+    ["ko'ylak (erkaklar)", "Hemd", "das", "Hemden"], ["ko'ylak (ayollar)", "Kleid", "das", "Kleider"],
+    ["shim", "Hose", "die", "Hosen"], ["kurtka", "Jacke", "die", "Jacken"],
+    ["poyabzal", "Schuh", "der", "Schuhe"], ["shlyapa", "Hut", "der", "Hüte"],
+    ["paypoq", "Socke", "die", "Socken"], ["qo'lqop", "Handschuh", "der", "Handschuhe"],
+    ["sharf", "Schal", "der", "Schals"], ["kamari", "Gürtel", "der", "Gürtel"],
+
+    # --- HAYVONLAR (Tiere) ---
+    ["it", "Hund", "der", "Hunde"], ["mushuk", "Katze", "die", "Katzen"],
+    ["ot", "Pferd", "das", "Pferde"], ["sigir", "Kuh", "die", "Kühe"],
+    ["tovuq", "Huhn", "das", "Hühner"], ["qo'y", "Schaf", "das", "Schafe"],
+    ["sher", "Löwe", "der", "Löwen"], ["ayiq", "Bär", "der", "Bären"],
+    ["qush", "Vogel", "der", "Vögel"], ["baliq", "Fisch", "der", "Fische"],
+
+    # --- TRANSPORT VA SHAHAR ---
+    ["velosiped", "Fahrrad", "das", "Fahrräder"], ["mototsikl", "Motorrad", "das", "Motorräder"],
+    ["samolyot", "Flugzeug", "das", "Flugzeuge"], ["poyezd", "Zug", "der", "Züge"],
+    ["kema", "Schiff", "das", "Schiffe"], ["bekat", "Haltestelle", "die", "Haltestellen"],
+    ["aeroport", "Flughafen", "der", "Flughäfen"], ["vokzal", "Bahnhof", "der", "Bahnhöfe"],
+    ["ko'prik", "Brücke", "die", "Brücken"], ["bino", "Gebäude", "das", "Gebäude"],
+
+    # --- TABIAT (Natur) ---
+    ["daraxt", "Baum", "der", "Bäume"], ["gul", "Blume", "die", "Blumen"],
+    ["o't", "Gras", "das", "-"], ["tosh", "Stein", "der", "Steine"],
+    ["osmon", "Himmel", "der", "-"], ["yomg'ir", "Regen", "der", "-"],
+    ["qor", "Schnee", "der", "-"], ["shamol", "Wind", "der", "-"],
+    ["o'rmon", "Wald", "der", "Wälder"], ["ko'l", "See", "der", "Seen"],
+
+    # --- MAKTAB VA ISH (Schule & Arbeit) ---
+    ["qalam", "Bleistift", "der", "Bleistifte"], ["ruchka", "Kugelschreiber", "der", "Kugelschreiber"],
+    ["daftar", "Heft", "das", "Hefte"], ["lug'at", "Wörterbuch", "das", "Wörterbücher"],
+    ["imtihon", "Prüfung", "die", "Prüfungen"], ["savol", "Frage", "die", "Fragen"],
+    ["javob", "Antwort", "die", "Antworten"], ["xato", "Fehler", "der", "Fehler"]
+]
+    # --- SHAHAR VA JOY NOMLARI (Orte) ---
+    ["dorixona", "Apotheke", "die", "Apotheken"], ["shifoxona", "Krankenhaus", "das", "Krankenhäuser"],
+    ["politsiya", "Polizei", "die", "-"], ["do'kon", "Laden", "der", "Läden"],
+    ["supermarket", "Supermarkt", "der", "Supermärkte"], ["restoran", "Restaurant", "das", "Restaurants"],
+    ["mehmonxona", "Hotel", "das", "Hotels"], ["bank", "Bank", "die", "Banken"],
+    ["teatr", "Theater", "das", "Theater"], ["muzey", "Museum", "das", "Museen"],
+    ["kutubxona", "Bibliothek", "die", "Bibliotheken"], ["maydon", "Platz", "der", "Plätze"],
+
+    # --- KASBLAR (Berufe) ---
+    ["shifokor", "Arzt", "der", "Ärzte"], ["hamshira", "Krankenschwester", "die", "Schwestern"],
+    ["haydovchi", "Fahrer", "der", "Fahrer"], ["oshpaz", "Koch", "der", "Köche"],
+    ["muhandis", "Ingenieur", "der", "Ingenieure"], ["sotuvchi", "Verkäufer", "der", "Verkäufer"],
+    ["politsiyachi", "Polizist", "der", "Polizisten"], ["talaba", "Student", "der", "Studenten"],
+    ["rassom", "Maler", "der", "Maler"], ["uchuvchi", "Pilot", "der", "Piloten"],
+
+    # --- SPORT VA O'YINLAR (Sport) ---
+    ["to'p", "Ball", "der", "Bälle"], ["futbol", "Fußball", "der", "-"],
+    ["suzish", "Schwimmen", "das", "-"], ["yugurish", "Laufen", "das", "-"],
+    ["velosport", "Radsport", "der", "-"], ["g'alaba", "Sieg", "der", "Siege"],
+    ["mag'lubiyat", "Niederlage", "die", "Niederlagen"], ["jamoa", "Mannschaft", "die", "Mannschaften"],
+
+    # --- HIS-TUYG'ULAR (Gefühle) ---
+    ["sevgi", "Liebe", "die", "-"], ["nafrat", "Hass", "der", "-"],
+    ["qo'rquv", "Angst", "die", "Ängste"], ["quvonch", "Freude", "die", "Freuden"],
+    ["g'azab", "Zorn", "der", "-"], ["tinchlik", "Frieden", "der", "-"],
+    ["ishonch", "Vertrauen", "das", "-"], ["umid", "Hoffnung", "die", "Hoffnungen"],
+
+    # --- RANGLAR (Yana qo'shimcha) ---
+    ["pushti", "rosa", "-", "-"], ["jigarrang", "braun", "-", "-"],
+    ["kulrang", "grau", "-", "-"], ["siyohrang", "lila", "-", "-"],
+    ["to'q sariq", "orange", "-", "-"], ["kumushrang", "silber", "-", "-"],
+    ["oltinrang", "gold", "-", "-"],
+
+    # --- MAVSUM VA OB-HAVO ---
+    ["bahor", "Frühling", "der", "-"], ["yoz", "Sommer", "der", "-"],
+    ["kuz", "Herbst", "der", "-"], ["qish", "Winter", "der", "-"],
+    ["quyoshli", "sonnig", "-", "-"], ["bulutli", "bewölkt", "-", "-"],
+    ["issiq", "warm", "-", "-"], ["sovuq", "kalt", "-", "-"],
+    ["nam", "nass", "-", "-"], ["quruq", "trocken", "-", "-"]
+]
