f1f1eb07b3b0b5ccb54d34c9cfff9f3b6ea5fc95
nurik81 <qalbdagigaplar01@gmail.com>
2026-05-14 13:41:59 +0500

    Update german_words.py

--- german_words.py
+++ german_words.py
@@ -3,6 +3,7 @@ de_data = [
     ["stol", "Tisch", "der", "Tische"],
     ["stul", "Stuhl", "der", "Stühle"],
     ["uy", "Haus", "das", "Häuser"],
+    ["maktab", "Schule", "die", "Schulen"],
     ["karovat", "Bett", "das", "Betten"],
     ["oyna", "Spiegel", "der", "Spiegel"]
 ]
